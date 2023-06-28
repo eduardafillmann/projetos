7fa8324d4ac388f1da86f862ca9362e560dcf94f
eduardafillmann <111399806+eduardafillmann@users.noreply.github.com>
2023-06-28 17:59:46 -0300

    update

---
+++ projeto 1/js/script.js
@@ -0,0 +1,31 @@
+// Seleçãi de elementos 
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+// Funções
+const saveTodo = (text) => {
+
+    const todo = document.createElement("div");
+    todo.classList.add("todo")
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    console.log(todo)
+};
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputValue = todoInput.value; //37:00 video
+
+    if (inputValue) {
+        saveTodo(inputValue)
+    }
+});
