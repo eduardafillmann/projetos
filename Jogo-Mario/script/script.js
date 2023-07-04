ee47ae2fe1e5e13f14c6b3f16ebc79da47f2f87a
eduardafillmann <111399806+eduardafillmann@users.noreply.github.com>
2023-07-04 13:16:23 -0300

    update final

--- Jogo-Mario/script/script.js
+++ Jogo-Mario/script/script.js
@@ -11,14 +11,14 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
-
+    
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     console.log(marioPosition);
 
-    if (pipePosition <= 120 && pipePosition > 0  &&marioPosition < 80) {
+    if (pipePosition <= 120 && pipePosition > 0  && marioPosition < 80) {
 
         pipe.style.animation = 'none'
         pipe.style.left = `${pipePosition}px`;
